37551dc79a60a93976fb5993a295c31c69f26cbe
Jesse <jessegoodrum@me.com>
2022-11-16 20:58:42 -0800

    added signup

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,16 @@
-import {signInWithGooglePopup, createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils.js'
-
+import {signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect, auth} from '../../utils/firebase/firebase.utils.js'
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component.jsx';
+// import { useEffect } from 'react';
+// import { getRedirectResult } from 'firebase/auth';
 
 export default function SignIn(){
+
+    // useEffect(async() => {
+    //        const response = await getRedirectResult(auth)
+    //        if(response) {
+    //         const userDocRef = await createUserDocumentFromAuth(response.user);
+    //        }
+    //     }, [])
 
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
@@ -9,10 +18,14 @@ export default function SignIn(){
     };
 
 
+
+
     return(
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>Sign In With Google Popup</button>
+            <SignUpForm />
+
         </div>
     )
 }
